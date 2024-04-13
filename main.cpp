b7dc05fef20caa83ed93dd1477425371b801dee3
Hikaru Kurusu <92407425+HikaruKurusu@users.noreply.github.com>
2024-04-13 14:53:10 -0700

    make window bigger

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main()
 
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 500, "GAME", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "GAME", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
