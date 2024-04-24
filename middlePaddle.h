d795c9067fd616dc2cc8c04cc6731315a6a8762f
Hikaru Kurusu <92407425+HikaruKurusu@users.noreply.github.com>
2024-04-24 13:03:32 -0700

    Added new paddle class for upcasting

--- middlePaddle.h
+++ middlePaddle.h
@@ -4,57 +4,23 @@
 #include "compPaddle.h"
 #ifndef MIDDLEPADDLE_H
 #define MIDDLEPADDLE_H
-class middlePaddle: protected compPaddle{
+class middlePaddle: protected CompPaddle{
     float paddleSize = 0.8f;
     float paddleSpeed;
     float paddleX;
     float paddleY; 
     float initialY;
 public:
-    middlePaddle(float speed, float posX, float posY) {
-        paddleSpeed = speed;
-        paddleX = posX;
-        paddleY = posY;
-        initialY = posY;
+    middlePaddle(float speed, float posX, float posY) : CompPaddle(speed, posX, posY) {
+    // Other initialization code for middlePaddle if needed
     }
     ~middlePaddle() {}
-
-    float getY() const {
-        return paddleY;
-    }
-
-    float getX() const{
-        return paddleX;
-    }
-
-    float getSize() const{
-        return paddleSize;
-    }
-
-    void setY(float y){
-        paddleY = y;
-    }
-
-    float getSpeed() const {
-        return paddleSpeed;
-    }
-
-    void draw_Paddle() {
-        glColor3f(1.0,1.0,1.0);
-        glLineWidth(10);
-
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-        glBegin(GL_QUADS);
-            glVertex2f(paddleX - 0.05f, paddleY);
-            glVertex2f(paddleX - (paddleSize/10.0f), paddleY);
-            glVertex2f(paddleX - (paddleSize/10.0f), paddleY - paddleSize/2.0f);
-            glVertex2f(paddleX - 0.05f, paddleY - paddleSize/2.0f);
-        glEnd();
-    }
-
-    void resetPaddle() {
-        paddleY = initialY;
+    void middleMovement(){
+        if(paddleY - paddleSize/2.0f > -1.0f) {
+            paddleY -= 10;
+        } else if(paddleY < 1.0f) {
+            paddleY += 10;
+        }
     }
 };
 #endif
