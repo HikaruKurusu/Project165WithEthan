f57dc9946a5c8e4495b6bcf830d3bbe82e5ac16a
something456 <eacvillarosa@gmail.com>
2024-04-24 13:07:02 -0700

    fixed middle paddle movement

--- middlePaddle.h
+++ middlePaddle.h
@@ -1,7 +1,7 @@
 #include "dependencies/include/glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <cmath>
-#include "compPaddle.h"
+#include "CompPaddle.h"
 #ifndef MIDDLEPADDLE_H
 #define MIDDLEPADDLE_H
 class middlePaddle: protected CompPaddle{
@@ -10,16 +10,23 @@ class middlePaddle: protected CompPaddle{
     float paddleX;
     float paddleY; 
     float initialY;
+    bool movingDown = true;
 public:
     middlePaddle(float speed, float posX, float posY) : CompPaddle(speed, posX, posY) {
     // Other initialization code for middlePaddle if needed
     }
     ~middlePaddle() {}
     void middleMovement(){
-        if(paddleY - paddleSize/2.0f > -1.0f) {
+        if(paddleY - paddleSize/2.0f > -1.0f && movingDown) {
             paddleY -= 10;
         } else if(paddleY < 1.0f) {
+            if (!movingDown) {
+                movingDown = !movingDown;
+            }
             paddleY += 10;
+        }
+        else {
+            movingDown = !movingDown;
         }
     }
 };
