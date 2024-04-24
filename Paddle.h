9bc7e36ac718d27875c6f776da234d0cb4c1d034
Hikaru Kurusu <92407425+HikaruKurusu@users.noreply.github.com>
2024-04-23 17:30:01 -0700

    polymorph and inheritance

--- Paddle.h
+++ Paddle.h
@@ -1,50 +1,12 @@
 #include "dependencies/include/glad/glad.h"
 #include <GLFW/glfw3.h>
 #include <cmath>
-class Paddle {
+class Paddle{
 public:
-    float paddleSize = 0.8f;
-    float paddleSpeed;
-    float paddleX;
-    float paddleY;//change these to make it on edge of screen
-    float initialY;
+    Paddle(float speed, float posX, float posY);
+    ~Paddle();
 
-    Paddle(float speed, float posX, float posY) {
-        paddleSpeed = speed;
-        paddleX = posX;
-        paddleY = posY;
-        initialY = posY;
-    }
-    ~Paddle() {}
-    void draw_Paddle_left() {
-        glColor3f(1.0,1.0,1.0);
-        glLineWidth(10);
+    virtual void draw_Paddle() = 0;
 
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-        glBegin(GL_QUADS);
-            glVertex2f(paddleX + 0.05f, paddleY);
-            glVertex2f(paddleX + (paddleSize/10.0f), paddleY);
-            glVertex2f(paddleX + (paddleSize/10.0f), paddleY - paddleSize/2.0f);
-            glVertex2f(paddleX + 0.05f, paddleY - paddleSize/2.0f);
-        glEnd();
-    }
-
-    void draw_Paddle_right() {
-        glColor3f(1.0,1.0,1.0);
-        glLineWidth(10);
-
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-        glBegin(GL_QUADS);
-            glVertex2f(paddleX - 0.05f, paddleY);
-            glVertex2f(paddleX - (paddleSize/10.0f), paddleY);
-            glVertex2f(paddleX - (paddleSize/10.0f), paddleY - paddleSize/2.0f);
-            glVertex2f(paddleX - 0.05f, paddleY - paddleSize/2.0f);
-        glEnd();
-    }
-
-    void resetPaddle() {
-        paddleY = initialY;
-    }
+    virtual void resetPaddle() = 0;
 };
--- PlayerPaddle.h
+++ PlayerPaddle.h
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <cmath>
 #include "Paddle.h"
-class compPaddle: public Paddle{
+class PlayerPaddle: public Paddle{
 public:
     float paddleSize = 0.8f;
     float paddleSpeed;
@@ -10,15 +10,29 @@ public:
     float paddleY;//change these to make it on edge of screen
     float initialY;
 
-    compPaddle(float speed, float posX, float posY) {
+    PlayerPaddle(float speed, float posX, float posY) {
         paddleSpeed = speed;
         paddleX = posX;
         paddleY = posY;
         initialY = posY;
     }
-    ~compPaddle() {}
+    ~PlayerPaddle() {}
 
     void draw_Paddle() {
+        glColor3f(1.0,1.0,1.0);
+        glLineWidth(10);
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+        glBegin(GL_QUADS);
+            glVertex2f(paddleX + 0.05f, paddleY);
+            glVertex2f(paddleX + (paddleSize/10.0f), paddleY);
+            glVertex2f(paddleX + (paddleSize/10.0f), paddleY - paddleSize/2.0f);
+            glVertex2f(paddleX + 0.05f, paddleY - paddleSize/2.0f);
+        glEnd();
+    }
+
+    void draw_Paddle_right() {
         glColor3f(1.0,1.0,1.0);
         glLineWidth(10);
 
